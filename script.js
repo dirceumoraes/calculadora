60fcec59f0dc04d2194f1a29797eb60bd45fb981
dirceumoraes <dirceumoraes142@gmail.com>
2026-05-21 22:36:18 -0400

    Adicionar html e css e js

---
+++ script.js
@@ -0,0 +1,20 @@
+let resultado = document.querySelector('#display');
+
+function insert(num) {
+	resultado.innerHTML += num;
+}
+
+function apagar() {
+	resultado.innerHTML = '';
+}
+
+
+function calcular() {
+	let resultado = document.querySelector('#display').innerHTML;
+
+	if(resultado !== '') {
+		document.querySelector('#display').innerHTML = eval(resultado);
+	} else {
+		document.querySelector('#display').innerHTML = 'Nada para calcular';
+	}
+}
